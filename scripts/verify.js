c6075f7bf2ace702550766fb3fe243c47fcb0080
Anatoly Antsupov <NAnatolyAntsupovN@yandex.ru>
2022-05-06 11:08:57 +0300

    rm addresses

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,11 +1,7 @@
 const network = hre.network.name;
 const fs = require("fs");
-const users = [
-  '0xddacF850692b1e284Fd585191101b272E1608Fb5', 
-  '0xC480B32B4f6354B1479524114d6b284B57D81117', 
-  '0xEA1615176598F531BD614BDf0Bbd0BadDca46a49'
-];
-const portions = [33, 33, 33];
+const users = [];
+const portions = [];
 
 async function main() {
   const dir = "./networks/";
